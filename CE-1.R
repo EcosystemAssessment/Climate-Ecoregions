b3e1278f78724cf0b0cfaea3211772cdc709ede4
Sparkle Malone <starrlab@sparkles-imac.rmrs.fs.fed.us>
2015-06-25 11:05:04 -0600

    Updated script

--- CE-1.R
+++ CE-1.R
@@ -2,18 +2,16 @@
 # Objectives: Import and format Shapefiles.
 # Shapefile Source (http://data.fs.usda.gov/geodata/edw/datasets.php)
 
-# Climate patterns for ecoregions
 rm(list=ls())
 
 library(rgdal)
 library(RCurl)
 library(raster)
-library(regeos)
+library(rgeos)
 
-setwd( '~/git/Climate-Ecoregions/ShapeFiles') # Adjust to your directory!
+setwd( '~/git/Climate-Ecoregions/ShapeFiles')
 
 # Import and plot shapefiles:
-
 region.rpa <-readOGR(".", 'RPA_region2') # RPA Region 2
 climate.summary <-readOGR(".", 'S_USA.ClimateSections') # Climate data by province
 er.p <-readOGR(".", 'S_USA.EcoMapProvinces') # Ecoregion Provinces
@@ -26,37 +24,19 @@ climate.summary <- spTransform(climate.summary, crs)
 er.p <- spTransform(er.p, crs)
 usa <- spTransform(usa, crs)
 
-# Vizualize of shapefile layers
-plot(climate.summary, border="lightgrey")
-plot(er.p, border="dimgrey", add=T)
-plot(usa, border="black", add=T)
-plot(region.rpa, border="blue", add=T)
 
-# Creat Domain and Division Shape files
-er.dom<- aggregate(er.p, vars= er.p$DOMAIN_NAM, dissolve=T)
-er.div<- aggregate(er.p, vars= er.p$DIVISION_N, dissolve=T)
+# Creat Domain and Division Shape files (Two methods of dissolving polygons)
+er.dom<- aggregate(er.p, vars= er.p$DOMAIN_NAM, dissolve=T) 
+er.div<- aggregate(er.p, vars= er.p$DIVISION_N, dissolve=T) # Not working
 
 library(maptools)
 er.dom<- unionSpatialPolygons(er.p, IDs=er.p$DOMAIN_NAM)
 er.div<- unionSpatialPolygons(er.p, IDs=er.p$DIVISION_N)
 
 # Figure:
-
 library(RColorBrewer)
 pal.2 <- colorRampPalette(c("gold", "green","skyblue", "grey", "blue"))
 
-library(ggplot2)
-er.p.f <- fortify(er.p, region="DIVISION_N")
-
-spplot(er.p, "DIVISION_N", col="white" ,par.settings=(fontfamily="mono"), col.regions=pal.2(12),
-       colorkey = T, bty="n", lwd=0.4, family="serif",
+spplot(er.p, "DIVISION_N", col="white", col.regions=pal.2(12),
+       colorkey = T, bty="n", lwd=0.4,
        sp.layout=list("sp.polygons", region.rpa, first=F))
-
-
-
-
-
-
-
-
-
